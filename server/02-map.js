e366169cdf6fa194f3f4141e2e44827eb72505c3
JDaniel <daniel@JDStart.onmicrosoft.com>
2023-11-28 12:50:55 +0000

    Ejemplo de uso de map

---
+++ server/02-map.js
@@ -0,0 +1,13 @@
+const letters = ['a', 'b', 'c'];
+
+const newArray = [];
+for (let index = 0; index < letters.length; index++) {
+    const element = letters[index];
+    newArray.push(element+ '++');
+}
+console.log('original', letters);
+console.log('new', newArray);
+
+const newArray2 = letters.map(item => item + '++');
+console.log('original con map', letters);
+console.log('new con map', newArray2);
