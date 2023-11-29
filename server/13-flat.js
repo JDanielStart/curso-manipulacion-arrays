6b022a1fc58c07ab27d6f97800413a38263b0c42
JDaniel <daniel@JDStart.onmicrosoft.com>
2023-11-29 12:22:43 +0000

    Uso de flat

---
+++ server/13-flat.js
@@ -0,0 +1,19 @@
+const matriz = [
+    [1,2,3],
+    [4,5,6, [1, 2, 3, 4, [2, 3, 4] ] ],
+    [7,8,9]
+];
+
+const newArray = [];
+for (let i = 0; i < matriz.length; i++) {
+    const array = matriz[i];
+    for (let j = 0; j < array.length; j++) {
+        const element = array[j];
+        newArray.push(element);
+    }
+}
+
+console.log('for', newArray);
+
+const rta = matriz.flat(3);
+console.log('flat', rta);
