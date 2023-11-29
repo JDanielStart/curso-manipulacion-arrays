4f3aa48b25812eab1cab3491ee6276736af9dfe8
JDaniel <daniel@JDStart.onmicrosoft.com>
2023-11-29 11:50:28 +0000

    Uso de join y split

---
+++ server/11-join-split.js
@@ -0,0 +1,16 @@
+const elements = ['Fire', 'Air', 'Water'];
+
+let rtaFinal = '';
+const separator = '__';
+for (let index = 0; index < elements.length; index++) {
+    const element = elements[index];
+    rtaFinal = rtaFinal + element + separator;
+}
+
+console.log('for', rtaFinal);
+const rta = elements.join('--');
+console.log('join', rta);
+
+const title = 'Curso de manipulación de arrays';
+
+console.log(title.split(' ').join('-').toLowerCase());
